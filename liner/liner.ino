b36471956f38fbe4c218941193d7b00a738e875c
Lancat <lancat1499@gmail.com>
2018-01-21 21:09:38 +0800

    完成設計圖

--- liner/liner.ino
+++ liner/liner.ino
@@ -10,15 +10,15 @@ int x;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(IR,INPUT);
+  pinMode(IR, INPUT);
   Serial.begin(9600);
   Serial.println("begin");
   delay(5000);
-  for(byte i=0; i<4; i++){
-    easymotor(200,200,1000);
-    easymotor(100,-100,1000);
+  for (byte i = 0; i < 4; i++) {
+    easymotor(200, 200, 1000);
+    easymotor(100, -100, 1000);
   }
-  setmotor(0,0);
+  setmotor(0, 0);
 }
 
 void loop() {
