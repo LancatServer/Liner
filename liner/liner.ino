7bc69073ab441f09fbe57194b441855fe5bd1f88
Lancat <lancat1499@gmail.com>
2018-01-24 21:23:04 +0800

    改成單感應器，測試失敗

--- liner/liner.ino
+++ liner/liner.ino
@@ -4,25 +4,36 @@
 #define L1 4
 #define L2 3
 #define FIX 0
-
-#define SPEED 80
 //Sensor right and left IR receiver
-#define RIR 50
+#define RIR A1
 #define LIR 51
 //button
 #define B1 45
-boolean r;
-boolean l;
+//run info
+#define MID 400
+const float MT = 0.2;
+#define SPEED 70
 
 boolean button(){
   return !digitalRead(B1);
 }
 
+float getvolue(){
+  float x = analogRead(RIR);
+  if(x < 50){
+    return 50;
+  }else if(x > 750){
+    return 750;
+  }
+  return x - MID;
+}
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(RIR, INPUT);
+  //pinMode(RIR, INPUT);
   pinMode(LIR, INPUT);
   pinMode(B1, INPUT);
+  digitalWrite(B1, HIGH);
   Serial.begin(9600);
   Serial.println("begin");
   while(!button()){}
@@ -30,16 +41,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  r = digitalRead(RIR);
-  l = digitalRead(LIR);
-  Serial.println(r, l);
-  if(r && !l){
-    setmotor(-1*SPEED, SPEED);
-  }else if(!r && l){
-    setmotor(SPEED, -1*SPEED);
-  }else{
-    setmotor(SPEED, SPEED);
-  }
+  float error = getvolue();
+  Serial.print("right:");
+  Serial.println(error);
+  delay(1000);
+  float turn = error * MT;
+  setmotor(SPEED+turn, SPEED-turn);
 }
 
 
